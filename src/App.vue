4ff569a6e868aade25552b47596ba1bb04fc6dfe
Matheus Salomão <mathesbh@gmail.com>
2021-06-03 14:33:29 -0300

    Lista casos brasil

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  
+<div>
+  <h2>Confirmados:</h2>{{ info.confirmed }}
+</div>
 </template>
 
 <script>
+import { Api } from './services/Api';
 
 export default {
   name: 'App',
   components: {
     
+  },
+  data(){
+    return{
+      info: null
+    }
+  },
+  mounted() {
+    Api.getBrazil().then(resp  => (this.info = resp.data.All));
   }
 }
 </script>
