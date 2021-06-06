6586b473a48d0d8d172babf78d116d83e138a524
Matheus Salomão <mathesbh@gmail.com>
2021-06-06 10:57:22 -0300

    Adiciona componente navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+    <h1>{{items.covid}} - {{items.title}} </h1>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  components: {
+
+  },
+  props: {
+    items: Object,
+  },
+  data(){
+    return {
+      
+    }
+  },
+}
+</script>
+
+<style>
+  nav{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 75px;
+    background-color: #f51219;
+    margin-bottom: 2%;
+  }
+
+  h1{
+    color: aliceblue;
+    font-size: xx-large;
+    font-weight: 700;
+  }
+
+</style>
