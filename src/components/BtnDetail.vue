e12ae280482e72938848e986d15724a216dcf84a
Matheus Salomão <mathesbh@gmail.com>
2021-06-06 14:31:37 -0300

    Adiciona componente botão detalhes

---
+++ src/components/BtnDetail.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button>{{btnTitle}}</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BtnDetail',
+  props: {
+    btnTitle: String,
+  }
+}
+</script>
+
+<style>
+  button{
+    width: 120px;
+    height: 28px;
+    background-color: #990000;
+    border: #990000;
+    border-radius: 15px;
+    color: aliceblue;
+    font-size: 1em;
+    cursor: pointer;
+  }
+
+  button:hover{
+    background-color: #f51219;
+  }
+
+</style>
