f46edcf2758b957ab4af6c4cd7b4ed823bbe86fb
Matheus Salomão <mathesbh@gmail.com>
2021-06-06 15:41:48 -0300

    Adiciona componente footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer></footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style>
+ footer{
+   height: 25px;
+   width: 100%;
+   background-color: #f51219;
+   bottom: 0;
+   position: fixed;
+ }
+
+</style>
