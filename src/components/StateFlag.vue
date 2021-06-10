8b87b3119b0a38154442f2afe9f60c786570221a
Matheus Salomão <mathesbh@gmail.com>
2021-06-10 16:15:42 -0300

    Adiciona componente stateflag

---
+++ src/components/StateFlag.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <img :src="flag" alt="" width="45" height="32">
+  </div>  
+</template>
+
+<script>
+
+export default {
+  name: 'StateFlag',
+  components: {
+
+  },
+  props: {
+    flag: String
+  },
+  data(){
+    return{
+
+    }
+  },
+  methods: {
+    getFlag(){
+      console.log(this.flag)
+    }
+  },
+}
+</script>
+
+<style scoped>
+  div{
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+  img{
+    border: 1px solid #888;
+    border-radius: 5px;
+  }
+
+</style>
