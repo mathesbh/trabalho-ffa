af129da27facb2dfef9d1553b87ee40183fc5bf5
Matheus Salomão <mathesbh@gmail.com>
2021-06-10 16:18:19 -0300

    Adiciona caminho imagens bandeira estados

---
+++ src/services/Flags.js
@@ -0,0 +1,110 @@
+export const Flags = [
+  {
+    uf: 'Acre',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/AC.png',
+  },
+  {
+    uf: 'Alagoas',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/AL.png',
+  },
+  {
+    uf: 'Amapa',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/AP.png',
+  },
+  {
+    uf: 'Amazonas',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/AM.png',
+  },
+  {
+    uf: 'Bahia',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/BA.png',
+  },
+  {
+    uf: 'Ceara',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/CE.png',
+  },
+  {
+    uf: 'Distrito Federal',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/DF.png',
+  },
+  {
+    uf: 'Espirito Santo',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/ES.png',
+  },
+  {
+    uf: 'Goias',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/GO.png',
+  },
+  {
+    uf: 'Maranhao',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/MA.png',
+  },
+  {
+    uf: 'Mato Grosso',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/MT.png',
+  },
+  {
+    uf: 'Mato Grosso do Sul',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/MS.png',
+  },
+  {
+    uf: 'Minas Gerais',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/MG.png',
+  },
+  {
+    uf: 'Para',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/PA.png',
+  },
+  {
+    uf: 'Paraiba',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/PB.png',
+  },
+  {
+    uf: 'Parana',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/PR.png',
+  },
+  {
+    uf: 'Pernambuco',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/PE.png',
+  },
+  {
+    uf: 'Piaui',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/PI.png',
+  },
+  {
+    uf: 'Rio de Janeiro',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/RJ.png',
+  },
+  {
+    uf: 'Rio Grande do Norte',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/RN.png',
+  },
+  {
+    uf: 'Rio Grande do Sul',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/RS.png',
+  },
+  {
+    uf: 'Rondonia',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/RO.png',
+  },
+  {
+    uf: 'Roraima',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/RR.png',
+  },
+  {
+    uf: 'Santa Catarina',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/SC.png',
+  },
+  {
+    uf: 'Sao Paulo',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/SP.png',
+  },
+  {
+    uf: 'Sergipe',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/SE.png',
+  },
+  {
+    uf: 'Tocantins',
+    flag: 'https://devarthurribeiro.github.io/covid19-brazil-api/static/flags/TO.png',
+  },
+]
